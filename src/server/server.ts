26da10981d633c2a2ec164bb1bb5d98be9eb11d3
Kelvin Van <kelvinvan.cs@gmail.com>
2022-12-19 15:08:47 -0800

    Completed TS boilerplate for front and backend
    Co-authored-by: Kelvin Van <90666201+KelvinVan1@users.noreply.github.com>
    Co-authored-by: Yufa Li <112290188+01001101CK@users.noreply.github.com>
    Co-authored-by: Emily Paine<22458780+erpaine@users.noreply.github.com>
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: Debbie Zavaleta <101748002+dzavaleta96@users.noreply.github.com>

---
+++ src/server/server.ts
@@ -0,0 +1,42 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import imageRouter from './routes/imageApi';
+
+const app = express();
+
+//Port constant
+const PORT = 3000;
+
+//Adding necessary parsing functions
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Having all routes be checked
+//app.use('/img', imageRouter)
+
+// Catch for invalid request
+// app.use('/*', (req: Request, res: Response) => {
+//   return res.sendStatus(404);
+// });
+
+// Global error catching
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  const defaultErr = {
+    log: `GLOBAL ERROR HANDLER: caught unknown middleware error${err.toString()}`,
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  if (errorObj.log) console.log(errorObj);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+//Spin up backend on port
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+export default app;
+  
