1fcea009a2c90349b48f2baa65541cce1591ece6
Kelvin Van <kelvinvan.cs@gmail.com>
2022-12-19 14:00:31 -0800

    Completed basic filestructure layout
    Co-authored-by: Kelvin Van <90666201+KelvinVan1@users.noreply.github.com>
    Co-authored-by: Yufa Li <112290188+01001101CK@users.noreply.github.com>
    Co-authored-by: Emily Paine<22458780+erpaine@users.noreply.github.com>
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: Debbie Zavaleta <101748002+dzavaleta96@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-    entry: './src/index.js',
+    entry: './src/client/index.js',
 
     output: {
         path: path.join(__dirname, '/dist'),
@@ -12,7 +12,7 @@ module.exports = {
 
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html'
+            template: './src/client/index.html'
         })
     ],
 
