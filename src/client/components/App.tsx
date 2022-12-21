ac0502dbd4e38f1239a795be523cef01845fe5df
Adeel Rajput <raj@adeels-mac-mini.myfiosgateway.com>
2022-12-20 20:10:19 -0500

    added embed spotify playlist according to button state
    Co-authored-by: Kelvin Van <90666201+KelvinVan1@users.noreply.github.com>
    Co-authored-by: Yufa Li <112290188+01001101CK@users.noreply.github.com>
    Co-authored-by: Emily Paine<22458780+erpaine@users.noreply.github.com>
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: Debbie Zavaleta <101748002+dzavaleta96@users.noreply.github.com>

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Mood from './Mood'
 import Image from './Image'
 import Music from './Music';
+// import Login from './Login';
+// import Signup from './Signup';
 
 export default function App () {
+    const [moodState,setMoodState] = useState(0)
     return (
         <div className='MainContainer'>
+            {/* <div className='LoginBox'>
+                <Login></Login>
+            </div>
+            <div className='SignupBox'>
+                <Signup></Signup>
+            </div> */}
              <div className='MoodBox'>
-                <Mood></Mood>
+                <Mood
+                moodState={moodState}
+                setMoodState={setMoodState}
+                ></Mood>
             </div>
             <div className='ImageBox'>
                 <Image></Image>
             </div>
             <div className='MusicBox'>
-                <Music></Music>
+                <Music
+                moodState={moodState}
+                setMoodState={setMoodState}
+                ></Music>
             </div>
         </div>
     )
